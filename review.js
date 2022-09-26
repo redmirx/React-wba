b759f99f4ae98067bf995b99d33ebf0cb19b5961
redmirx <redmirx9@gmail.com>
2022-09-26 14:41:33 +0500

    lesson-2

---
+++ review.js
@@ -0,0 +1,13 @@
+///////////////////// lesson 1 /////////////////////
+
+// npx create-react-app
+// package.json, src, public
+// single page application
+// React virtual DOM
+// ReactDOM.render()
+// What is JSX
+// import, export
+
+///////////////////// lesson 2 /////////////////////
+
+// Props, components
