9d697332a7698eede3f5ac10e009369c13faaef2
redmirx <redmirx9@gmail.com>
2022-09-27 11:01:27 +0500

    lesson-3

---
+++ state/src/State.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class State extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello State</h1>
+      </div>
+    );
+  }
+}
+
+export default State;
