0cec72a4067cd3bdeaf953e5020768d7df9fd13e
redmirx <redmirx9@gmail.com>
2022-09-27 12:04:43 +0500

    lesson-3

--- state/src/State.jsx
+++ state/src/State.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 class State extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
   render() {
+    const plus = () => {};
+    const minus = () => {};
     return (
       <div>
-        <h1>Hello State</h1>
+        <h1>State {this.state.count}</h1>
+        <button onClick={plus}>+</button>
+        <button onClick={minus}>-</button>
       </div>
     );
   }
