51cff1844fe282cb14a9164bb2c6f4d422a2b5ef
redmirx <redmirx9@gmail.com>
2022-09-28 23:59:48 +0500

    lesson-2

--- props_and_components/src/index.js
+++ props_and_components/src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const students = [
-  { id: 1, name: "Abdurakhmon" },
-  { id: 2, name: "Abdulloh" },
-  { id: 1, name: "Abdurakhmon" },
-  { id: 2, name: "Abdulloh" },
-  { id: 1, name: "Abdurakhmon" },
-  { id: 2, name: "Abdulloh" },
   { id: 1, name: "Abdurakhmon" },
   { id: 2, name: "Abdulloh" },
   { id: 1, name: "Abdurakhmon" },
@@ -24,8 +16,3 @@ root.render(
     ))}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
