0a8fd7ec21eea018b4c68c5c91a7e1e6a64a63a5
redmirx <redmirx9@gmail.com>
2022-09-28 23:55:17 +0500

    lesson-4

--- function-call_and_multi-state-change/src/App.jsx
+++ function-call_and_multi-state-change/src/App.jsx
@@ -27,11 +27,6 @@ class App extends Component {
       // console.log(result);
       this.setState({ data: result });
     };
-    const onDelete = (id) => {
-      console.log(id);
-      let result = this.state.data.filter((value) => value.id !== id);
-      this.setState({ data: result });
-    };
     return (
       <div>
         <h1>{this.state.title}</h1>
@@ -57,48 +52,16 @@ class App extends Component {
           placeholder="Change the surname"
         />
         <hr />
-        <center>
-          <input onChange={onFilter} type="text" placeholder="Filter by name" />
-          <table border={1} width="50%">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Delete</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.data.length ? (
-                this.state.data.map((value) => {
-                  return (
-                    <tr key={value.id}>
-                      <td>{value.id}</td>
-                      <td>{value.name}</td>
-                      <td>{value.status}</td>
-                      <td>
-                        <button onClick={() => onDelete(value.id)}>
-                          Delete
-                        </button>
-                      </td>
-                      <td>
-                        <button>Edit</button>
-                      </td>
-                    </tr>
-                  );
-                })
-              ) : (
-                <tr>
-                  <th colSpan={5}>No data</th>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </center>
+        <input onChange={onFilter} type="text" placeholder="Filter by name" />
+        {this.state.data.map((value) => {
+          return (
+            <h1 key={value.id}>
+              {value.id}. {value.name} ({value.status})
+            </h1>
+          );
+        })}
       </div>
     );
   }
 }
-
 export default App;
