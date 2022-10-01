34d67aa66555f5d040d16b799cc58acdba020bd6
redmirx <redmirx9@gmail.com>
2022-10-01 13:23:32 +0500

    lesson-6

---
+++ styled_components/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Container } from "./AppStyle";
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Styled Components</h1>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ styled_components/src/AppStyle.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled("div")`
+  background-color: springgreen;
+  border: 2px solid coral;
+`;
+
+export { Container };
