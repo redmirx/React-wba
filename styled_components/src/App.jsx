50630d26a247da7676211914b37dbd9af28afb1c
redmirx <redmirx9@gmail.com>
2022-10-01 17:18:08 +0500

    lesson-6

--- styled_components/src/App.jsx
+++ styled_components/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
-import { Container, Box } from "./AppStyle";
+import { Container, Title, Box, Desc } from "./AppStyle";
 class App extends Component {
   render() {
     return (
       <Container>
-        <Box type="large">Large</Box>
-        <Box type="medium">Medium</Box>
-        <Box type="small">Small</Box>
+        <Box bgColor="coral" type="large">
+          Large
+        </Box>
+        <Box bgColor="yellow" type="medium">
+          Medium
+        </Box>
+        <Box bgColor="white" type="small">
+          Small
+        </Box>
+        <Title>Hey there</Title>
+        <Desc left>Description 1</Desc>
+        <Desc>Description 2</Desc>
       </Container>
     );
   }
--- styled_components/src/AppStyle.jsx
+++ styled_components/src/AppStyle.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled("div")`
   display: flex;
@@ -6,6 +6,26 @@ export const Container = styled("div")`
   background-color: springgreen;
   border: 2px solid coral;
   padding: 20px;
+`;
+
+const common = css`
+  height: fit-content;
+  font-weight: 400;
+  font-family: sans-serif;
+  color: #222;
+  border: 2px solid #222;
+  padding: 5px;
+  font-size: 28px;
+`;
+
+export const Title = styled.div`
+  ${common};
+`;
+
+export const Desc = styled.div`
+  ${common};
+  font-size: 18px; // The last line of code will be applied
+  color: ${({ left }) => (left ? "red" : "green")};
 `;
 
 function getSize(props) {
@@ -22,10 +42,13 @@ function getSize(props) {
 }
 
 export const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   display: flex;
   justify-content: center;
   align-items: center;
   width: ${getSize};
   height: ${getSize};
   border: 5px solid violet;
+  font-size: 22px;
+  font-family: sans-serif;
 `;
